481d5d2733df02c034d54af1b4d580f2ecd6e1cd
Shrijan <justforyouutubee@gmail.com>
2025-02-04 16:38:22 +0530

    First Commit

--- app/src/main/java/com/example/birthdayapplication/MainActivity.kt
+++ app/src/main/java/com/example/birthdayapplication/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     BirthdayApplicationTheme {
-        Greeting("Shrijan")
+        Greeting("Shrijan ")
     }
 }
