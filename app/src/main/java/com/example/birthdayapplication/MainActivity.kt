690cec89ab334a93321bb268794ea335bf83a114
Shrijan <justforyouutubee@gmail.com>
2025-02-08 19:27:24 +0530

    Used padding and alignment

--- app/src/main/java/com/example/birthdayapplication/MainActivity.kt
+++ app/src/main/java/com/example/birthdayapplication/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.birthdayapplication.ui.theme.BirthdayApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +29,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BirthdayApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )
+                {
+                    GreetingText(message = "Happy Birthday Shrijan!" , from = "From Android" , modifier = Modifier.padding(8.dp))
                 }
             }
         }
@@ -27,16 +42,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String , modifier: Modifier = Modifier) {
-    Text(
-        text = message
-    )
+fun GreetingText(message: String , from : String , modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp) , verticalArrangement = Arrangement.Center){
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp).align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true , name = "My preview" , showSystemUi = false)
 @Composable
 fun BirthdayCardPreview() {
     BirthdayApplicationTheme {
-        GreetingText(message = "Happy Birthday Shrijan!")
+        GreetingText(message = "Happy Birthday Shrijan!" , from = "From Android")
     }
 }
